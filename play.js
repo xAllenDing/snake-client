794f6a0368c9c186aa961eb2d72290a29499a3ad
Allen Ding <allen.ding@hotmail.com>
2023-01-18 18:02:33 -0800

    created an event handler to exit the game

--- play.js
+++ play.js
@@ -1,4 +1,23 @@
 const {connect} = require('./client');
 
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  const handleUserInput = function (data) {
+      if (data === '\u0003') {
+        console.log("terminated");
+        process.exit();
+        
+      }
+  }
+  stdin.on('data', handleUserInput);
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
 console.log("Connecting ...");
 connect();
+setupInput();
