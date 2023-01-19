28b16fc6bd1c9d20fbcd97650ed93759e9e5f957
Allen Ding <allen.ding@hotmail.com>
2023-01-18 19:02:49 -0800

    made a constants file for hard codes

--- client.js
+++ client.js
@@ -1,10 +1,10 @@
 const net = require("net");
-
+const {IP, PORT} = require('./constants');
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: "165.227.47.243",// IP address here,
-    port: "50541"// PORT number here,
+   IP,// IP address here,
+   PORT// PORT number here,
   });
 
   //message after connecting
