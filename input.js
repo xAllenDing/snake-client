014e54851574a07c6ab4bb84c8d60702d0b5cffc
Allen Ding <allen.ding@hotmail.com>
2023-01-18 18:50:07 -0800

    added movement to snek game

--- input.js
+++ input.js
@@ -1,14 +1,34 @@
-
+// const { connect } = require("./client");
+let connection;
 
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
+
   const handleUserInput = function (data) {
     if (data === '\u0003') {
-      console.log("terminated");
+      // console.log("terminated");
       process.exit();
     }
+    if (data === 'w') {
+      // console.log("you have entered w");
+      connection.write("Move: up");
+    }
+    if (data === 'a') {
+      // console.log("you have entered a");
+      connection.write("Move: left");
+    }
+    if (data === 's') {
+      // console.log("you have entered s");
+      connection.write("Move: down");
+    }
+    if (data === 'd') {
+      // console.log("you have entered d");
+      connection.write("Move: right");
+    }
   }
+
   stdin.on('data', handleUserInput);
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
--- play.js
+++ play.js
@@ -2,5 +2,5 @@ const { connect } = require('./client');
 const { setupInput } = require('./input');
 
 console.log("Connecting ...");
-connect();
-setupInput();
+let conn = connect();
+setupInput(conn);
